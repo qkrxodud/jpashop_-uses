0640d624ab4566fa5b9a7cdd84a0a90d1cde67fc
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-11 15:20:22 +0900

    *아이템로직 구현

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,19 +1,47 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 @DiscriminatorColumn(name="dtype")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Item {
+
     @Id @GeneratedValue
     @Column(name = "item_id")
     private Long id;
+
     private String name;
     private int price;
-    private int StockQuantity;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직==//
+    /**
+     * Stcok 증가
+     */
+    public void addStock(int quantity) {
+        stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int result = stockQuantity - quantity;
+        if (result<0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = result;
+    }
 }
