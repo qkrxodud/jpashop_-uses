fa6556f35b120ec274637071dda3cfcc8dbb5ee7
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-11 17:51:40 +0900

    *주문 로직 추가

---
+++ src/main/java/jpabook/jpashop/domain/service/OrderService.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.domain.repository.ItemRepository;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import jpabook.jpashop.domain.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+    private final OrderRepository orderRepository;
+
+    /**
+     * 즈믄
+     */
+    @Transactional
+    public Long order(Long MemberId, Long itemId, int count) {
+
+        //엔티티 조회
+        Member member = memberRepository.findOne(MemberId);
+        Item item = itemRepository.findOne(itemId);
+
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        //주문 저장
+        orderRepository.save(order);
+
+        return  order.getId();
+    }
+
+    /**
+     * 주문취소
+     */
+    @Transactional
+    public  void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
+
+
+}
