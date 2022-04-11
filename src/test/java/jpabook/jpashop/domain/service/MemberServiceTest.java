52dab0d908853fe33b0340e6e42c8d06ae11e3a3
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-11 14:06:23 +0900

    *회원로직 구현  - 테스트 케이스 작성

---
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertThat(member.getId()).isEqualTo(saveId);
+    }
+    
+    @Test
+    public void 중복회원예외() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        //when
+        memberService.join(member);
+
+        //then
+        org.junit.jupiter.api.Assertions.assertThrows(IllegalStateException.class, ()-> {
+            memberService.join(member2);
+        });
+    }
+
+}
