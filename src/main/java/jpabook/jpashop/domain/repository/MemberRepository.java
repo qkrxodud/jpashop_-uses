bff35b191a9b61a3fd97bfca5c593f4eb2960c3c
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-11 17:50:03 +0900

    *회원 단일조회 추가

--- src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.repository;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -23,6 +24,10 @@ public class MemberRepository {
 
     public List<Member> findAll() {
         return em.createQuery("select m from Member m").getResultList();
+    }
+
+    public Member findOne(Long memberId) {
+        return em.find(Member.class, memberId);
     }
 
     public List<Member> findByName(String name) {
