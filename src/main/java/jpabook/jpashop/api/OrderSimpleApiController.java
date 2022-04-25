c7cacc7261043b88e9bd9abf398550a8e5522918
qkrxodud00 <qkrxodud00@gmail.com>
2022-04-25 15:08:16 +0900

    *simple-ordersV1  - 문제점 : Entity를 직접 전달하기 때문에 양방향 연관관계에서 무한 루프로 빠진다.  - 해결방법 : DTO를 만들고 값들을 직접 넣어주자. *simple-ordersV2  - 문제점 : n + 1의 문제가 발생,    1. 지연 로딩을 통해서 주문 조회    2. 주문을 통해서 멤버 2명 조회 : 1번의 쿼리    3. Straem을 통해서 2명의 멤버 조회 : 2번의 쿼리    4. Stream 을 통해서 Address를 2번 조회 : 2번의 쿼리    총 5번의 쿼리가 발생  - 해결병밥 : 디패치를 사용해서 쿼리를 조회하자.

---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,77 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * xToOne
+ * order
+ * Order -< Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public Result ordersV2() {
+        // ODER 2개
+        // N + 1 -> 1 + 회원 N(2) + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> collect = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getMember().getAddress();
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+}
+
+
